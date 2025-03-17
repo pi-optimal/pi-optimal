441109b53da83a40ad79272ca1b26ebc66759033
Jochen Luithardt <koobaalt@cb77c9c8-98da-499b-8220-7bad41fe7efa.fritz.box>
2025-03-17 10:39:50 +0100

    feat: option to use the history or only the next state for reward prediction

--- pi_optimal/models/sklearn/base_sklearn_model.py
+++ pi_optimal/models/sklearn/base_sklearn_model.py
@@ -6,23 +6,50 @@ from torch.utils.data import DataLoader
 from pi_optimal.models.base_model import BaseModel
 
 class BaseSklearnModel(BaseModel):
-    
+    def __init__(self, **params):
+        """
+        Initialize the model.
+
+        Parameters:
+            use_past_states_for_reward (bool): Determines the input used for reward prediction.
+                - False: Use only the predicted next state (non-reward part).
+                - True:  Use a concatenation of the past input X and the predicted next state.
+                Defaults to False.
+            Other parameters may be passed via **params.
+        """
+        super().__init__(**params)
+        self.params = params
+        self.use_past_states_for_reward = params.get("use_past_states_for_reward", False)
+      
+    def fit(self, dataset):
+        """Fits the model to the dataset."""
+        raise NotImplementedError
+
     def predict(self, X):
         X = np.array(X, dtype=np.float32)
 
         # Predict all features except the reward
-        next_state = []
+        next_state_pred = []
         for i, model in enumerate(self.models):
             if i != self.dataset_config["reward_feature_idx"]:
                 feature_next_state = model.predict(X)
-                next_state.append(feature_next_state)
+                next_state_pred.append(feature_next_state)
         
-        # Predict the reward from the next state
-        next_state = np.array(next_state).T
-        reward_idx = self.dataset_config["reward_vector_idx"]
-        reward = self.models[self.dataset_config["reward_feature_idx"]].predict(next_state)
-        next_state = np.insert(next_state, reward_idx, reward, axis=1)
+        # Convert predictions into a (n_samples, n_features_without_reward) array
+        next_state_pred = np.array(next_state_pred).T
 
+        if self.use_past_states_for_reward:
+            # Concatenate the past input with the predicted next state to predict the reward.   
+            reward_input = np.concatenate([X, next_state_pred], axis=1)
+        else:
+            # Use only the predicted next state to predict the reward.
+            reward_input = next_state_pred
+
+        reward_idx = self.dataset_config["reward_vector_idx"]
+        reward = self.models[self.dataset_config["reward_feature_idx"]].predict(reward_input)
+        
+        # Insert the predicted reward back into the next state array at the configured position
+        next_state = np.insert(next_state_pred, reward_idx, reward, axis=1)
         return next_state
 
     def forward(self, state, action):
@@ -34,7 +61,6 @@ class BaseSklearnModel(BaseModel):
         assert inital_state.shape[0] == actions.shape[0]
         assert actions.shape[1] == n_steps
          
-
         state = inital_state
         next_states = []
         for i in range(n_steps):
@@ -42,23 +68,23 @@ class BaseSklearnModel(BaseModel):
             next_state = self.forward(state, action)
             next_states.append([next_state])
             state = np.roll(state, -1, axis=1)
-            state[:,-1] = next_state
+            state[:, -1] = next_state
         next_states = np.array(next_states)
         next_states = np.transpose(next_states, (2, 0, 1, 3))
         return next_states
         
     def save(self, filepath):
-            """Secure model saving with metadata and pickle"""
-            save_data = {
-                "models": self.models,
-                "dataset_config": self.dataset_config,
-                "params": self.params,
-                "model_type": self.__class__.__name__,
-                "model_config": getattr(self, 'model_config', None)
-            }
+        """Secure model saving with metadata and pickle"""
+        save_data = {
+            "models": self.models,
+            "dataset_config": self.dataset_config,
+            "params": self.params,
+            "model_type": self.__class__.__name__,
+            "model_config": getattr(self, 'model_config', None)
+        }
             
-            with open(filepath, 'wb') as f:
-                pickle.dump(save_data, f, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(filepath, 'wb') as f:
+            pickle.dump(save_data, f, protocol=pickle.HIGHEST_PROTOCOL)
 
     @classmethod
     def load(cls, filepath):
@@ -68,12 +94,8 @@ class BaseSklearnModel(BaseModel):
         
         if data.get('model_type') != cls.__name__:
             raise ValueError(f"Model type mismatch: Expected {cls.__name__}, got {data.get('model_type')}")
-
-        if data.get('model_type') == 'HybridModel':      
-            instance = cls(params=data["params"])
-        else:
-            instance = cls(**data["params"])
-
+            
+        instance = cls(**data["params"])
         instance.models = data["models"]
         instance.dataset_config = data["dataset_config"]
         if 'model_config' in data:
@@ -96,17 +118,15 @@ class BaseSklearnModel(BaseModel):
         feature_end_idx = feature["feature_end_idx"]
         return y[:, feature_begin_idx:feature_end_idx].ravel()
 
-        
     def fit(self, dataset):
-
         self.dataset_config = dataset.dataset_config
 
         dataloader = DataLoader(
             dataset, batch_size=len(dataset), shuffle=False, num_workers=0
         )
         past_states, past_actions, future_states, _ = next(iter(dataloader))
-        X = self._prepare_input_data(past_states, past_actions)
-        y = self._prepare_target_data(future_states)
+        states = self._prepare_input_data(past_states, past_actions)
+        next_states = self._prepare_target_data(future_states)
 
         self.dataset_config = dataloader.dataset.dataset_config
 
@@ -115,14 +135,20 @@ class BaseSklearnModel(BaseModel):
             for state_idx in self.dataset_config["states"]
         ]
 
-        # Fit all models except the reward model
+        # Fit the models
         for i, model in enumerate(tqdm(self.models, desc="Training models...")):
             if i != self.dataset_config["reward_feature_idx"]:
-                y_target = self._get_target_for_feature(y, i)
-                model.fit(X, y_target)
+                next_state_target = self._get_target_for_feature(next_states, i)
+                model.fit(states, next_state_target)
             else:
                 reward_idx = self.dataset_config["reward_vector_idx"]
-                target_reward = self._get_target_for_feature(y, i)
-                next_state = y
-                next_state_wo_reward = np.delete(next_state, reward_idx, axis=1)
-                model.fit(next_state_wo_reward, target_reward)
+                target_reward = self._get_target_for_feature(next_states, i)
+                # Remove the reward from the next state to predict the reward.
+                next_state_wo_reward = np.delete(next_states, reward_idx, axis=1)
+
+                if self.use_past_states_for_reward:
+                    # Concatenate the past input with the next state without reward to predict the reward.
+                    states_w_next_states_wo_reward = np.concatenate([states, next_state_wo_reward], axis=1)
+                    model.fit(states_w_next_states_wo_reward, target_reward)
+                else:
+                    model.fit(next_state_wo_reward, target_reward)
