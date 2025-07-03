9928d8c0dc14dd06117be7ffa8359a3fd92691b3
Jochen Luithardt <jol@pi-optimal.com>
2025-07-03 11:25:23 +0200

    fix: predicting the current state
    The bug was that we start doing the rollout always from the step before and not the current timestep (this is due to the dataset class) so to hot fix this we predicting the current state and then start the rollout.

--- pi_optimal/planners/cem_discrete.py
+++ pi_optimal/planners/cem_discrete.py
@@ -28,6 +28,13 @@ class CEMDiscretePlanner(CEMPlanner):
         num_models = len(models)
         model_predictions = [[] for _ in range(num_models)]
 
+        next_states = models[0].forward(states, action_history)
+        model_predictions[0].append(next_states)
+
+        # Update states for next timestep (using the first model as reference)
+        states = np.roll(states, shift=-1, axis=1)
+        states[:, -1, :] = model_predictions[0][-1]
+        
         for t in tqdm(range(self.horizon), desc="Simulating trajectories"):
             current_actions = actions[:, t]
             current_actions_one_hot = np.zeros((self.population_size, self.action_dim))
